a3148401fcfe328aa45976a945019e29bc0ec7ba
sriram <sriramachandran28@gmail.com>
2025-03-24 12:52:11 -0300

    spring boot template

---
+++ src/main/java/com/example/hoteltransylvania/hotel_transylvania/HotelTransylvaniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoteltransylvania.hotel_transylvania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelTransylvaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelTransylvaniaApplication.class, args);
+	}
+
+}
