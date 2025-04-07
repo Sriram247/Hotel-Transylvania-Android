d282a00225346d04051ccdbaab30acc356eac8c2
sriram <sriramachandran28@gmail.com>
2025-04-07 15:27:14 -0300

    ✨ feat: update HomeScreen to include location parameter

--- app/src/main/java/com/example/hoteltransylvania/MainActivity.kt
+++ app/src/main/java/com/example/hoteltransylvania/MainActivity.kt
@@ -4,18 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.hoteltransylvania.ui.screens.HomeScreen
-import com.example.hoteltransylvania.ui.screens.HotelListActivity
 import com.example.hoteltransylvania.ui.theme.HotelTransylvaniaTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,11 +12,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HotelTransylvaniaTheme() {
-                HomeScreen { checkIn, checkOut, guests ->
+                HomeScreen { location ,checkIn, checkOut, guests ->
                     // Save to Room (next step)
                     // Make GraphQL service call (later)
 
-                    val intent = Intent(this, Class.forName("com.example.hoteltransylvania.ui.screens.HotelListActivity"))
+                    val intent = Intent(this, Class.forName("com.example.hoteltransylvania.ui.screens.HotelListActivity.kt"))
+                    intent.putExtra("location", location)
                     intent.putExtra("checkIn", checkIn)
                     intent.putExtra("checkOut", checkOut)
                     intent.putExtra("guests", guests)
